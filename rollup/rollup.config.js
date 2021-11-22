d9e93f748254998a052de4e40d08cc1ffc8ea4cd
kaiserdc <koreshdc@gmail.com>
2021-11-22 22:02:41 +0200

    Task02

--- rollup/rollup.config.js
+++ rollup/rollup.config.js
@@ -1,7 +1,24 @@
+import { babel } from '@rollup/plugin-babel';
+import image from 'rollup-plugin-img';
+import styles from "rollup-plugin-styles";
+import serve from 'rollup-plugin-serve'
+import livereload from 'rollup-plugin-livereload'
+
 export default {
-    input: './src/index.js',
-    output: {
-      file: './dist/bundle.js',
-      format: 'cjs'
-    },
-  };
+  input: './src/index.js',
+  output: {
+    file: './dist/bundle.js',
+    format: 'cjs'
+  },
+  plugins: [
+    babel({ babelHelpers: 'bundled' }),
+    image({ limit: 100000 }),
+    styles(),
+    serve({
+      open: true,
+      contentBase: '',
+      port: 3310,
+    }),
+    livereload()
+  ]
+};
--- rollup/src/index.js
+++ rollup/src/index.js
@@ -1,2 +1,11 @@
+import '../src/index.css'
+
+const h1Title = document.createElement('h1')
+h1Title.innerText = 'I love JavaScript'
+document.body.append(h1Title)
+const jsLogoImage = document.createElement('img')
+jsLogoImage.classList.add('js-logo')
+jsLogoImage.src = '../assets/240px-JavaScript-logo.png'
+document.body.append(jsLogoImage)
 console.log('Hello World!');
 console.log('Goodbye World!');

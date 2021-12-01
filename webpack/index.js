7c177b4051bf4f4e0276903a4255c4c3e9bd03f1
kaiserdc <koreshdc@gmail.com>
2021-12-01 22:55:20 +0200

    task 04

--- webpack/index.js
+++ webpack/index.js
@@ -1,1 +1,12 @@
-console.log(`Hello World!`);
+import './assets/index.css'
+import JS_LOGO from './assets/240px-JavaScript-logo.png'
+
+const h1Title = document.createElement('h1')
+h1Title.innerText = 'I love JavaScript'
+document.body.append(h1Title)
+const jsLogoImage = document.createElement('img')
+jsLogoImage.classList.add('js-logo')
+jsLogoImage.src = JS_LOGO
+document.body.append(jsLogoImage)
+console.log('Hello World!');
+console.log('Goodbye World!');
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,3 +1,5 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const path = require('path');
 
 module.exports = {
@@ -6,5 +8,32 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'index.html'
+        }),
+        new CleanWebpackPlugin(),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ['babel-loader'],
+            },
+            {
+                test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+                type: 'asset/inline',
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
     }
 };
